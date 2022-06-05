524f0ebc37c8c2e77040c2f22877ef43461507c9
Supun Kavinda <supun@hemnette.com>
2022-06-05 14:58:56 +0530

    initial commit

---
+++ src/test/java/com/assignment/prowerplantsystem/ProwerPlantSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.prowerplantsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProwerPlantSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
