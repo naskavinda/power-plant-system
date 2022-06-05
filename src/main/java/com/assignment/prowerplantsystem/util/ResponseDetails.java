3f98397d64a7a925a0b70bfa3b2412ef83e8e268
Supun Kavinda <supun@hemnette.com>
2022-06-05 22:52:27 +0530

    fixed issue in ResponseDetails class

--- src/main/java/com/assignment/prowerplantsystem/util/ResponseDetails.java
+++ src/main/java/com/assignment/prowerplantsystem/util/ResponseDetails.java
@@ -2,8 +2,8 @@ package com.assignment.prowerplantsystem.util;
 
 public final class ResponseDetails {
 
-    public static final ResponseDetails E1001 = new ResponseDetails("E1000", "Records are already in the data base.");
-    public static final ResponseDetails E1002 = new ResponseDetails("E1000", "Request contain duplicate names.");
+    public static final ResponseDetails E1001 = new ResponseDetails("E1001", "Records are already in the data base.");
+    public static final ResponseDetails E1002 = new ResponseDetails("E1002", "Request contain duplicate names.");
 
 
     private String code;
