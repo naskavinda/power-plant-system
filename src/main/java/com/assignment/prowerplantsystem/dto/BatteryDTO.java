59648dcdbf1e4c3bf993a3fecf8798b6697c9f78
Supun Kavinda <supun@hemnette.com>
2022-06-05 17:59:24 +0530

    create battery save flow

--- src/main/java/com/assignment/prowerplantsystem/dto/BatteryDTO.java
+++ src/main/java/com/assignment/prowerplantsystem/dto/BatteryDTO.java
@@ -1,23 +1,15 @@
-package com.assignment.prowerplantsystem.entity;
+package com.assignment.prowerplantsystem.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table
-@Entity
-public class Battery {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+public class BatteryDTO {
 
     private String name;
     private String postcode;
